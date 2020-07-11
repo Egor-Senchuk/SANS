75a77e481e96a577a44e909671bb51a0e5d65c5e
noob!?! <egor.s04092812@mail.ru>
2020-07-11 17:05:07 +0300

    Add files via upload

---
+++ _SANS_/sanslexer.py
@@ -0,0 +1,15 @@
+
+
+
+
+def lexer():
+
+
+
+
+#tokens
+string = input(':>>> ')
+tokens = string.split(' ')
+
+print(tokens)
+#tokens
---
+++ _SANS_/shell.py
@@ -0,0 +1,45 @@
+
+import sanslexer
+import os
+os.system('clear')
+
+
+print('SANS 1.0.0')
+
+
+#1.input path file 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
